a5fd4c85bb21c9101bb0de26c8bc9c9717532088
erinfox <erinkayfox@gmail.com>
2026-04-08 10:13:21 -0700

    add Jest, React Testing Library, and jest-axe

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/fileMock.cjs",
+  },
+};
